d48ff7d58159bc26e6ae10c8dd8485f37e44dea7
Kent Brekke <kamundsen@live.no>
2024-10-21 08:31:12 +0200

    Rydder kommentarer og skrivefeil

--- main.py
+++ main.py
@@ -19,34 +19,38 @@ def main():
     
     '''
     Sammenligner priser for begge bil typene
+    Bruk av operatører og funksjoner:
+        - {:.2f}  for å formattere float, tilsier 2 desimaler etter komma
+        - f - string eller formatted string literals for å bruke replacement fields
+        - abs - absolutt verdi, konverteres til positiv verdi eller 0
     '''
     # Forsikring
     insurance_difference = abs(electrical_car.insurance - gasoline_car.insurance)
     if electrical_car.insurance < gasoline_car.insurance:
-        print(f"El-bil har billigere forsikrings kostnader: {electrical_car.insurance:.2f} kr | {insurance_difference:.2f} kr billigere enn bensin")
+        print(f"El-bil har billigere forsikrings kostnader: {electrical_car.insurance:.2f} kr | Differanse: {insurance_difference:.2f} kr billigere enn bensin") 
     else:
-        print(f"Bensin bil har billigere forsikrings kostnader: {gasoline_car.insurance:.2f} kr | {insurance_difference:.2f} kr billigere enn elektrisk")
+        print(f"Bensin bil har billigere forsikrings kostnader: {gasoline_car.insurance:.2f} kr | Differanse: {insurance_difference:.2f} kr billigere enn elektrisk")
 
     # Drivstoff
     fuel_difference = abs(electrical_car.fuel_cost - gasoline_car.fuel_cost)
     if electrical_car.fuel_cost < gasoline_car.fuel_cost:
-        print(f"El-bil har billigere drivstoff kostnader: {electrical_car.fuel_cost:.2f} kr | {fuel_difference:.2f} kr billigere enn bensin")
+        print(f"El-bil har billigere drivstoff kostnader: {electrical_car.fuel_cost:.2f} kr | Differanse: {fuel_difference:.2f} kr billigere enn bensin")
     else:
-        print(f"Bensin bil har billigere drivstoff kostnader: {gasoline_car.fuel_cost:.2f} kr | {fuel_difference:.2f} kr billigere enn elektrisk")
+        print(f"Bensin bil har billigere drivstoff kostnader: {gasoline_car.fuel_cost:.2f} kr | Differanse: {fuel_difference:.2f} kr billigere enn elektrisk")
 
     # Bom avgift
     toll_difference = abs(electrical_car.toll - gasoline_car.toll)
     if electrical_car.toll < gasoline_car.toll:
-        print(f"El-bil har billigere bom avgift: {electrical_car.toll:.2f} kr | {toll_difference:.2f} kr billigere enn bensin")
+        print(f"El-bil har billigere bom avgift: {electrical_car.toll:.2f} kr | Differanse: {toll_difference:.2f} kr billigere enn bensin")
     else:
-        print(f"Bensin bil har billigere bom avgift: {gasoline_car.toll:.2f} kr | {toll_difference:.2f} kr billigere enn elektrisk")
+        print(f"Bensin bil har billigere bom avgift: {gasoline_car.toll:.2f} kr | Differanse: {toll_difference:.2f} kr billigere enn elektrisk")
 
     # Årlig totale kostnader
     total_cost_difference = abs(electrical_car.annual_cost - gasoline_car.annual_cost)
     if electrical_car.annual_cost < gasoline_car.annual_cost:
-        print(f"El-bil er total billigere: {electrical_car.annual_cost:.2f} | {total_cost_difference:.2f} kr billigere enn bensin")
+        print(f"El-bil er totalt billigere: {electrical_car.annual_cost:.2f} | Differanse: {total_cost_difference:.2f} kr billigere enn bensin")
     else:
-        print(f"Bensin bil er totalt billigere: {gasoline_car.annual_cost:.2f} kr | {total_cost_difference:.2f} kr billigere enn elektrisk")
+        print(f"Bensin bil er totalt billigere: {gasoline_car.annual_cost:.2f} kr | Differanse: {total_cost_difference:.2f} kr billigere enn elektrisk")
 
 if __name__ == '__main__':
     main()
